67a030e010d1f068b5e476f3d2c7651ac05d574b
Killer31g <szymon192016@outlook.com>
2023-01-09 11:27:28 +0100

    koniec lekcji

--- klasa-4d-2022-2023-main/Pierwszy projekt/ClockWpfApp/ViewModels/StopwatchVM.cs
+++ klasa-4d-2022-2023-main/Pierwszy projekt/ClockWpfApp/ViewModels/StopwatchVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using UtilitiesWpf;
@@ -10,6 +11,8 @@ namespace ClockWpfApp.ViewModels
 {
     class StopwatchVM : ObserverVM
     {
+        private bool isStopWatchRunning = false;
+
 		private string _timeStorage;
 
 		public string TimeStorage
@@ -28,13 +31,33 @@ namespace ClockWpfApp.ViewModels
 		{
 			get 
 			{
-				if (_startCommand == null)
-					_startCommand = new RelayCommand<object>(
-						o =>
-						{
+                if (_startCommand == null)
+                    _startCommand = new RelayCommand<object>(
+                        o =>
+                        {
+                            isStopWatchRunning = true;
+                            Task.Run(() =>
+                            {
+                                DateTime startTime = DateTime.Now;
+                                //int stopwatchMillisecond = 0;
+                                while (isStopWatchRunning)
+                                {
+                                    //stopwatchMillisecond++;
+                                    //TimeSpan timeSpan = TimeSpan.FromMilliseconds(stopwatchMillisecond);
 
-						});
-				return _startCommand;
+                                    TimeSpan timeSpan = DateTime.Now - startTime;
+                                    string stringTime = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
+                                        timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
+
+                                    TimeStorage = stringTime;
+
+                                    Thread.Sleep(1);
+                                }
+                            });
+                        },
+                        o => !isStopWatchRunning
+                        );
+                return _startCommand;
 			}
 		}
         private ICommand _stopCommand;
@@ -47,11 +70,41 @@ namespace ClockWpfApp.ViewModels
                     _stopCommand = new RelayCommand<object>(
                         o =>
                         {
-
-                        });
+                            isStopWatchRunning = false;
+                        },
+                        o => isStopWatchRunning
+                        );
                 return _stopCommand;
             }
         }
 
-    }
+		private ICommand _flagCommand;
+
+		public ICommand FlagCommand
+		{
+            get
+            {
+                if (_flagCommand == null)
+                    _flagCommand = new RelayCommand<object>(
+                        o =>
+                        {
+
+                        });
+                return _flagCommand;
+            }
+        }
+
+		private string _flagValue;
+
+		public string FlagValue
+		{
+            get { return _flagValue; }
+            set
+            {
+                _flagValue = value;
+                OnPropertyChanged(nameof(FlagValue));
+            }
+        }
+
+	}
 }
---
+++ klasa-4d-2022-2023-main/Pierwszy projekt/PhotoViewerWPFApp/PhotoViewerVM.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UtilitiesWpf;
+
+namespace PhotoViewerWPFApp
+{
+    internal class PhotoViewerVM : ObserverVM
+    {
+		private string imagePath;
+
+		public string ImagePath
+        {
+			get { return imagePath; }
+			set 
+			{
+				imagePath = value;
+				OnPropertyChanged(nameof(ImagePath));
+			}
+		}
+
+		private ICommand loadFromFileCommand;
+
+		public ICommand LoadFromFileCommand
+		{
+			get
+			{
+				if (loadFromFileCommand == null)
+					loadFromFileCommand = new RelayCommand<object>(
+						x =>
+						{
+							OpenFileDialog openFileDialog= new OpenFileDialog();
+							openFileDialog.Filter = "Obrazy JPG|*.jpg|Obrazy PNG|*.png|Obrazy JPEG|*.jpeg";
+							if(openFileDialog.ShowDialog() == true)
+							{
+								ImagePath = openFileDialog.FileName;
+							}
+						}
+						);
+				return loadFromFileCommand;
+			}
+		}
+
+
+	}
+}
