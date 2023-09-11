73b98c240c07d2411283c80a20909b2a16e48f33
Killer31g <szymon192016@outlook.com>
2023-09-11 13:17:54 +0200

    koniec lekcji

---
+++ klasa-4d-2022-2023-main/Pierwszy projekt/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿//unsigned int a,b;
+uint a, b;
+
+Console.WriteLine("Podaj a:");
+a = uint.Parse(Console.ReadLine());
+
+Console.WriteLine("Podaj b:");
+b = uint.Parse(Console.ReadLine());
+
+a = NWD(a, b);
+Console.WriteLine(a);
+
+uint NWD(uint a, uint b)
+{
+    while(a != b)
+    {
+    if (a > b)
+    a = a - b;
+    else
+    b = b - a;    
+    }
+return a;
+};
