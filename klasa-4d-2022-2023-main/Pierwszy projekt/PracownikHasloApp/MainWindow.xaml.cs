0617e23f1bce0a10453a5484e243bbeddd26184f
Killer31g <szymon192016@outlook.com>
2023-09-13 14:16:55 +0200

    koniec lekcji

--- klasa-4d-2022-2023-main/Pierwszy projekt/PracownikHasloApp/MainWindow.xaml.cs
+++ klasa-4d-2022-2023-main/Pierwszy projekt/PracownikHasloApp/MainWindow.xaml.cs
@@ -20,9 +20,92 @@ namespace PracownikHasloApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string generatedPassword;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonGeneratedPassword_Click(object sender, RoutedEventArgs e)
+        {
+            byte passwordLength;
+            
+            if(!byte.TryParse(textboxPasswordLength.Text, out passwordLength))
+            {
+                MessageBox.Show("Długość hasła jest niepoprawna");
+                return;
+            }
+
+            bool? isCapitalLetter = checkBoxCapitalLetter.IsChecked;
+            bool? isDigit = checkBoxDigit.IsChecked;
+            bool? isSpecialChar = checkBoxSpecialChar.IsChecked;
+
+            byte minPasswordLength = 1;
+            if (isCapitalLetter == true)
+            {
+                minPasswordLength++;
+            }
+            if (isDigit == true)
+            {
+                minPasswordLength++;
+            }
+            if (isSpecialChar == true)
+            {
+                minPasswordLength++;
+            }
+            if (passwordLength < minPasswordLength)
+            {
+                MessageBox.Show("Długość hasła jest za krótka");
+                return;
+            }
+
+            string smallLettersSet = "qwertyuiopasdfghjklzxcvbnm";
+            string capitalLettersSet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string digitsSet = "1234567890";
+            string specialCharsSet = "!@#$%^&*()_+-=";
+
+            generatedPassword = string.Empty;
+            Random random = new Random();
+            if (isCapitalLetter == true)
+            {
+                char randomChar = capitalLettersSet[random.Next(0, capitalLettersSet.Length)];
+                generatedPassword += randomChar;
+            }
+            if (isDigit == true)
+            {
+                char randomChar = digitsSet[random.Next(0, digitsSet.Length)];
+                generatedPassword += randomChar;
+            }
+            if (isSpecialChar == true)
+            {
+                char randomChar = specialCharsSet[random.Next(0, specialCharsSet.Length)];
+                generatedPassword += randomChar;
+            }
+
+            int missingNumberOfSmallLetters = passwordLength - generatedPassword.Length;
+            
+            for(int i = 0; i < missingNumberOfSmallLetters; i++)
+            {
+                char randomChar = smallLettersSet[random.Next(0, smallLettersSet.Length)];
+                generatedPassword += randomChar;
+            }
+            
+            /*generatedPassword += smallLettersSet.OrderBy(c => random.Next()).Take(missingNumberOfSmallLetters).ToString();*/
+
+            MessageBox.Show(generatedPassword);
+        }
+
+        private void ButtonConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Dane pracownika: \n";
+
+            message += "Imie i nazwisko: " + textBoxName.Text + "\n";
+            message += textBoxSurname.Text + "\n";
+            message += "Stanowisko: " + comboBoxJob.Text + "\n";
+            message += "Hasło: " + generatedPassword;
+
+            MessageBox.Show(message);
         }
     }
 }
