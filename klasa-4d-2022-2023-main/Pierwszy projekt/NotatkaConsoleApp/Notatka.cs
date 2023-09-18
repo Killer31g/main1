a493868f6f0a96581ff3d29459d8701681181482
Killer31g <szymon192016@outlook.com>
2023-09-18 13:14:15 +0200

    koniec lekcji

---
+++ klasa-4d-2022-2023-main/Pierwszy projekt/NotatkaConsoleApp/Notatka.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotatkaConsoleApp
+{
+    internal class Notatka
+    {
+        private static int liczbaNotatek;
+        private int id;
+        private string tytul;
+        private string tresc;
+        public Notatka(string tytul, string tresc)
+        {
+            liczbaNotatek++;
+            id = liczbaNotatek;
+            this.tytul = tytul;
+            this.tresc = tresc;
+        }
+
+        public void WyswietlNotatke()
+        {
+            Console.WriteLine("Tytuł: " + tytul);
+            Console.WriteLine($"Treść: {tresc}");
+        }
+
+        public void Diagnostyczna()
+        {
+            Console.WriteLine($"{liczbaNotatek};{id};{tytul};{tresc}");
+        }
+    }
+}
---
+++ klasa-4d-2022-2023-main/Pierwszy projekt/NotatkaConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using NotatkaConsoleApp;
+
+Notatka pierwszaNotatka = new Notatka("Tytuł pierwszej notatki",
+                                      "Treść pierwszej notatki");
+Notatka drugaNotatka = new Notatka("Tytuł drugiej notatki",
+                                   "Treść drugiej notatki");
+
+pierwszaNotatka.WyswietlNotatke();
+pierwszaNotatka.Diagnostyczna();
+
+drugaNotatka.WyswietlNotatke();
+drugaNotatka.Diagnostyczna();
