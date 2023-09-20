a2998acba9887b132bb496feaada97c662b5cb58
Killer31g <szymon192016@outlook.com>
2023-09-20 14:15:49 +0200

    koniec lekcji

--- klasa-4d-2022-2023-main/Pierwszy projekt/ListaNotatekMobileApp/ListaNotatekMobileApp/MainPage.xaml.cs
+++ klasa-4d-2022-2023-main/Pierwszy projekt/ListaNotatekMobileApp/ListaNotatekMobileApp/MainPage.xaml.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ListaNotatekMobileApp
 {
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<string> simpleTextNotesCollection;
+        public ObservableCollection<string> SimpleTextNotesCollection
+        {
+            get {  return simpleTextNotesCollection; }
+            set 
+            {
+                simpleTextNotesCollection = value;
+                OnPropertyChanged(nameof(SimpleTextNotesCollection));
+            }
+        }
+
+        private ICommand addNoteCommand;
+
+        public ICommand AddNoteCommand
+        {
+            get 
+            {
+                if (addNoteCommand == null)
+                    addNoteCommand = new Command<object>(
+                        o =>
+                        {
+                            SimpleTextNotesCollection.Add(entryNewNote.Text);
+                        });
+                return addNoteCommand;
+            }
+        }
+
+
         public MainPage()
         {
+            SimpleTextNotesCollection = new ObservableCollection<string>();
+            SimpleTextNotesCollection.Add("Zakupy: chleb, masło, ser");
+            SimpleTextNotesCollection.Add("Do zrobienia: obiad, umyc podłogi");
+            SimpleTextNotesCollection.Add("Weekend: kino, spacer z psem");
+
             InitializeComponent();
         }
     }
