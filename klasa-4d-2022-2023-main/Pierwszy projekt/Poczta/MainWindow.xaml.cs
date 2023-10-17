2ab7b244248c21b32ba42df7a55105f57a1bc725
Killer31g <szymon192016@outlook.com>
2023-10-17 22:22:24 +0200

    dobra robota

--- klasa-4d-2022-2023-main/Pierwszy projekt/Poczta/MainWindow.xaml.cs
+++ klasa-4d-2022-2023-main/Pierwszy projekt/Poczta/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Poczta
+namespace PocztaApp
 {
     public partial class MainWindow : Window
     {
@@ -22,54 +11,68 @@ namespace Poczta
             InitializeComponent();
         }
 
-
         private void SprawdzCeneButton_Click(object sender, RoutedEventArgs e)
         {
-            string wybranaOpcja = "";
-
-            if (PocztowkaRadioButton.IsChecked == true)
-            {
-                wybranaOpcja = "Pocztówka";
-                CenaLabel.Content = "Cena: 1 zł";
-                CenaLabel.Visibility = Visibility.Visible;
-            }
-            else if (ListRadioButton.IsChecked == true)
-            {
-                wybranaOpcja = "List";
-                CenaLabel.Content = "Cena: 1,5 zł";
-                CenaLabel.Visibility = Visibility.Visible;
-            }
-            else if (PaczkaRadioButton.IsChecked == true)
-            {
-                wybranaOpcja = "Paczka";
-                CenaLabel.Content = "Cena: 10 zł";
-                CenaLabel.Visibility = Visibility.Visible;
-            }
-
+            string wybranaOpcja = WybranaOpcja();
+            CenaLabel.Content = $"Cena: {ObliczCene(wybranaOpcja)} zł";
+            CenaLabel.Visibility = Visibility.Visible;
             MessageBox.Show($"Wybrano opcję: {wybranaOpcja}");
+        }
+
+        private string WybranaOpcja()
+        {
+            if (PocztowkaRadioButton.IsChecked == true)
+                return "Pocztówka";
+            if (ListRadioButton.IsChecked == true)
+                return "List";
+            if (PaczkaRadioButton.IsChecked == true)
+                return "Paczka";
+            return string.Empty;
+        }
+
+        private decimal ObliczCene(string opcja)
+        {
+            switch (opcja)
+            {
+                case "Pocztówka":
+                    return 1.0m;
+                case "List":
+                    return 1.5m;
+                case "Paczka":
+                    return 10.0m;
+                default:
+                    return 0.0m;
+            }
         }
 
         private void ZatwierdzButton_Click(object sender, RoutedEventArgs e)
         {
             string kodPocztowy = KodPocztowyTextBox.Text;
 
-            if (kodPocztowy.Length != 5)
-            {
-                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
-            }
-            else if (!IsNumeric(kodPocztowy))
-            {
-                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
-            }
-            else
+            if (SprawdzKodPocztowy(kodPocztowy))
             {
                 MessageBox.Show("Dane przesyłki zostały wprowadzone");
             }
         }
 
-        private bool IsNumeric(string input)
+        private bool SprawdzKodPocztowy(string kod)
         {
-            foreach (char c in input)
+            if (kod.Length != 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+                return false;
+            }
+            if (!CzyLiczby(kod))
+            {
+                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
+                return false;
+            }
+            return true;
+        }
+
+        private bool CzyLiczby(string tekst)
+        {
+            foreach (char c in tekst)
             {
                 if (!char.IsDigit(c))
                 {
